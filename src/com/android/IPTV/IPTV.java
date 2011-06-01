b5bf279040c7a7523dd113aa18ef4fd3cf57998e
Essoussi <Essoussi@Essoussi-PC.maisel.int-evry.fr>
2011-06-01 15:51:35 +0200

    First release

---
+++ src/com/android/IPTV/IPTV.java
@@ -0,0 +1,148 @@
+package com.android.IPTV;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class IPTV extends Activity implements View.OnClickListener,
+		View.OnKeyListener, AdapterView.OnItemSelectedListener {
+
+	private String URL = "";
+	private EditText editUrl;
+	private Spinner spin;
+	private int positionSpinner;
+	private ProgressDialog dialog;
+	private Button submitButton;
+
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		setContentView(R.layout.main);
+		
+		dialog = new ProgressDialog(this);
+		dialog.setCancelable(false);
+		dialog.setMessage("Connecting Wifi");
+		dialog.show();
+		
+		ConnectionManagement cm = new ConnectionManagement();
+		cm.execute((Void)null);
+		
+
+		// Bouton lecture vidéo
+		
+		submitButton = (Button) findViewById(R.id.buttonSubmit);
+		submitButton.setOnClickListener(this);
+		
+		// Zone de saisie + écoute évènement clavier
+		editUrl = (EditText) findViewById(R.id.urlEdit);
+		editUrl.setSingleLine(true);
+		editUrl.setOnKeyListener(this);
+
+		// La liste des modes (scénarios) du spinner
+		String[] scenarios = { "Web TV (HTTP)", "Live RTSP Streaming" };
+		
+		spin = (Spinner) findViewById(R.id.spinner_scenario);
+		
+		ArrayAdapter<String> aa = new ArrayAdapter<String>(this,
+				android.R.layout.simple_spinner_item, scenarios);
+		aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spin.setAdapter(aa);
+
+	}
+
+	public void onClick(View view) {
+
+		URL = editUrl.getText().toString();
+		if (URL.equals(""))
+			Toast.makeText(this, "Please enter a correct video link !",
+					Toast.LENGTH_SHORT).show();
+		else {
+			// création de l'activité lecture vidéo
+			Intent i = new Intent(this, PlayingVideo.class);
+
+			Bundle objetbunble = new Bundle();
+			objetbunble.putString("URL", URL);
+			objetbunble.putInt("position", positionSpinner);
+			i.putExtras(objetbunble);
+
+			startActivity(i);
+		}
+
+	}
+
+	public void onItemSelected(AdapterView<?> parent, View v, int position,
+			long id) {
+
+		if (position == 0) {
+			positionSpinner = 0;
+
+		} else {
+			positionSpinner = 1;
+
+		}
+
+	}
+
+	public void onNothingSelected(AdapterView<?> parent) {
+		spin.setSelection(0);
+		positionSpinner = 0;
+	}
+
+	public boolean onKey(View view, int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_ENTER) {
+			onClick(view);
+			return true;
+		} else
+			return false;
+	}
+
+	private class ConnectionManagement extends
+			AsyncTask<Void, Void, Void> {
+
+		
+
+		public void startWifi() {
+			WifiManager wM = (WifiManager) getSystemService(WIFI_SERVICE);
+			wM.setWifiEnabled(true);
+			
+		}
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			
+			WifiManager wm = (WifiManager) getSystemService(WIFI_SERVICE);
+			if (wm.getWifiState() == WifiManager.WIFI_STATE_DISABLED) {
+				startWifi(); // Démarrer le wifi dans le cas où il est désactivé
+					
+				for (int i = 0; i < 99; i++) {
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					
+				}
+			}
+			
+			return null;
+		}
+
+		
+
+		public void onPostExecute(Void param) {
+				dialog.cancel();
+
+		}
+	}
+}
---
+++ src/com/android/IPTV/PlayingVideo.java
@@ -0,0 +1,57 @@
+package com.android.IPTV;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.media.MediaPlayer.OnErrorListener;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+public class PlayingVideo extends Activity implements OnCompletionListener, OnErrorListener {
+
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		setContentView(R.layout.play_video);
+        
+		String URL = this.getIntent().getExtras().getString("URL");
+		int positionSpinner = this.getIntent().getExtras().getInt("position");
+
+		VideoView videoView = (VideoView) findViewById(R.id.videoPlayer);
+		videoView.setOnCompletionListener(this);
+		videoView.setOnErrorListener(this);
+		MediaController mediaController = new MediaController(this);
+		mediaController.setMediaPlayer(videoView);
+		if (positionSpinner == 0) // Web TV HTTP
+			videoView.setVideoPath(URL);
+		else
+			videoView.setVideoURI(Uri.parse(URL)); // RTSP: streaming
+
+		videoView.setMediaController(mediaController);
+		videoView.requestFocus();
+		videoView.start();
+		mediaController.show();
+
+	}
+
+	@Override
+	public void onCompletion(MediaPlayer arg0) {
+		
+		Intent i = new Intent(this, IPTV.class);
+		startActivity(i);
+	}
+
+	@Override
+	public boolean onError(MediaPlayer arg0, int arg1, int arg2) {
+		Toast.makeText(
+				this,
+				"ERROR: cannot read any video via this link",
+				Toast.LENGTH_SHORT).show();
+		onCompletion(arg0);
+		return false;
+	}
+}
