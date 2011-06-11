558e065c824f607cf124d30e25bc47209abc05a2
Essoussi <Essoussi@Essoussi-PC.maisel.int-evry.fr>
2011-06-11 19:15:35 +0200

    IPTV 1.2

--- gen/com/android/IPTV/R.java
+++ gen/com/android/IPTV/R.java
@@ -8,6 +8,10 @@
 package com.android.IPTV;
 
 public final class R {
+    public static final class anim {
+        public static final int layout_right_to_left=0x7f040000;
+        public static final int layout_right_to_left_config=0x7f040001;
+    }
     public static final class attr {
     }
     public static final class drawable {
@@ -16,51 +20,52 @@ public final class R {
         public static final int bfm=0x7f020002;
         public static final int btn_custom=0x7f020003;
         public static final int btn_custom_tv=0x7f020004;
-        public static final int direct8=0x7f020005;
-        public static final int france2=0x7f020006;
-        public static final int france3=0x7f020007;
-        public static final int france4=0x7f020008;
-        public static final int france5=0x7f020009;
-        public static final int icon=0x7f02000a;
-        public static final int itele=0x7f02000b;
-        public static final int lcp=0x7f02000c;
-        public static final int logo=0x7f02000d;
-        public static final int m6=0x7f02000e;
-        public static final int nrj12=0x7f02000f;
-        public static final int nt1=0x7f020010;
-        public static final int tf1=0x7f020011;
-        public static final int tmc=0x7f020012;
-        public static final int tsp=0x7f020013;
-        public static final int tv_button=0x7f020014;
-        public static final int tv_button_clicked=0x7f020015;
-        public static final int w9=0x7f020016;
+        public static final int button_blue=0x7f020005;
+        public static final int button_green=0x7f020006;
+        public static final int button_purple=0x7f020007;
+        public static final int direct8=0x7f020008;
+        public static final int france2=0x7f020009;
+        public static final int france3=0x7f02000a;
+        public static final int france4=0x7f02000b;
+        public static final int france5=0x7f02000c;
+        public static final int icon=0x7f02000d;
+        public static final int itele=0x7f02000e;
+        public static final int lcp=0x7f02000f;
+        public static final int logo=0x7f020010;
+        public static final int m6=0x7f020011;
+        public static final int nrj12=0x7f020012;
+        public static final int nt1=0x7f020013;
+        public static final int tf1=0x7f020014;
+        public static final int tmc=0x7f020015;
+        public static final int tsp=0x7f020016;
+        public static final int w9=0x7f020017;
     }
     public static final class id {
-        public static final int ServerAddress=0x7f070000;
-        public static final int ServerConfig=0x7f070018;
-        public static final int ServerEditText=0x7f070001;
-        public static final int arte=0x7f07000d;
-        public static final int bfm=0x7f070014;
-        public static final int buttonManual=0x7f070016;
-        public static final int buttonSubmit=0x7f070006;
-        public static final int direct8=0x7f07000e;
-        public static final int france2=0x7f070008;
-        public static final int france3=0x7f070009;
-        public static final int france4=0x7f07000a;
-        public static final int france5=0x7f07000b;
-        public static final int imageView1=0x7f070002;
-        public static final int itele=0x7f070015;
-        public static final int lcp=0x7f070013;
-        public static final int m6=0x7f07000c;
-        public static final int nrj12=0x7f070012;
-        public static final int nt1=0x7f070011;
-        public static final int spinner_scenario=0x7f070004;
-        public static final int tf1=0x7f070007;
-        public static final int tmc=0x7f070010;
-        public static final int urlEdit=0x7f070005;
-        public static final int videoPlayer=0x7f070017;
-        public static final int videolink=0x7f070003;
-        public static final int w9=0x7f07000f;
+        public static final int ServerAddress=0x7f080000;
+        public static final int ServerConfig=0x7f080018;
+        public static final int ServerEditText=0x7f080001;
+        public static final int arte=0x7f08000d;
+        public static final int bfm=0x7f080014;
+        public static final int buttonManual=0x7f080016;
+        public static final int buttonSubmit=0x7f080006;
+        public static final int direct8=0x7f08000e;
+        public static final int france2=0x7f080008;
+        public static final int france3=0x7f080009;
+        public static final int france4=0x7f08000a;
+        public static final int france5=0x7f08000b;
+        public static final int imageView1=0x7f080002;
+        public static final int itele=0x7f080015;
+        public static final int lcp=0x7f080013;
+        public static final int m6=0x7f08000c;
+        public static final int nrj12=0x7f080012;
+        public static final int nt1=0x7f080011;
+        public static final int spinner_scenario=0x7f080004;
+        public static final int tf1=0x7f080007;
+        public static final int tmc=0x7f080010;
+        public static final int urlEdit=0x7f080005;
+        public static final int videoPlayer=0x7f080017;
+        public static final int videolink=0x7f080003;
+        public static final int w9=0x7f08000f;
     }
     public static final class layout {
         public static final int alertdialogperso=0x7f030000;
@@ -69,16 +74,16 @@ public final class R {
         public static final int play_video=0x7f030003;
     }
     public static final class menu {
-        public static final int server_menu=0x7f060000;
+        public static final int server_menu=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
+        public static final int app_name=0x7f050000;
     }
     public static final class style {
-        public static final int Button=0x7f050000;
-        public static final int CustomApplication=0x7f050004;
-        public static final int EditText=0x7f050002;
-        public static final int ImageButton=0x7f050003;
-        public static final int TextView=0x7f050001;
+        public static final int Button=0x7f060000;
+        public static final int CustomApplication=0x7f060004;
+        public static final int EditText=0x7f060002;
+        public static final int ImageButton=0x7f060003;
+        public static final int TextView=0x7f060001;
     }
 }
